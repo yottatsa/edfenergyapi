9f38dccc922ade73478626d94c0c2aa42223b4b2
Vladimir Eremin <me@yottatsa.name>
2019-06-14 13:17:50 +0100

    json api for hourly graphs

--- edfenergyapi/__main__.py
+++ edfenergyapi/__main__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import datetime
 import os
 import json
 
@@ -8,8 +9,11 @@ from .api import EDF
 AUTH_FILE = ".edf.json"
 
 
-def print_readings(edf, p="year"):
-    readings = edf.request("myaccount/energygraph/{}".format(p))["data"][0]["result"]["readings"]
+def print_readings(edf, p="year", json=None):
+    if json:
+        readings = edf.session.get(json).json()[0]["result"]["readings"]
+    else:
+        readings = edf.request("myaccount/energygraph/{}".format(p))["data"][0]["result"]["readings"]
     if isinstance(readings, dict):
         readings = readings.values()
     p = lambda v: v['periodStart']
@@ -19,12 +23,16 @@ def print_readings(edf, p="year"):
         print(p(v), c(v))
         total += c(v)
     print(total)
+    return p(v), total
 
 
 if __name__ == "__main__" and os.path.exists(AUTH_FILE):
     with open(AUTH_FILE, "r") as f:
         auth = json.load(f)
     edf = EDF(**auth)
-    print_readings(edf)
-    print_readings(edf, "month")
+    _, total = print_readings(edf)
+    last_day, month_total = print_readings(edf, "month")
+    next_day = datetime.datetime.strptime(last_day, '%Y-%m-%d').date() + datetime.timedelta(days=1)
+    _, next_day_total = print_readings(edf, json="https://my.edfenergy.com/smartdata/{}/day/0".format(next_day))
+    print(total + month_total + next_day_total)
     edf.save()
